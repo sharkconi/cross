1452742260f6be6ce2b5a21f6a3934ede432d086
Mark.Ma <mam22@emc.com>
2016-09-13 10:38:34 +0800

    Add renewfee page and enforce the basic function
    Signed-off-by: Mark.Ma <mam22@emc.com>

--- app/admin/views.py
+++ app/admin/views.py
@@ -4,7 +4,7 @@
 
 from . import admin
 from .. import db
-from .forms import CreateUserForm
+from .forms import CreateUserForm, RenewfeeForm
 from ..models import User, Userlog, Package
 from flask import render_template, session, redirect, url_for, request
 from datetime import datetime
@@ -12,7 +12,51 @@ from dateutil.relativedelta import *
 
 @admin.route("/")
 def admin_main():
-     return render_template("admin/main.html")
+     users = []
+     for user in User.query.all():
+         traffics = 0
+         for log in Userlog.query.filter_by(user_id=user.id).all():
+             traffics = traffics + log.traffic
+         if traffics > 1024*1024:
+             traffics = str(traffics/1024/1024) + "MB"
+         elif traffics > 1024:
+             traffics = str(traffics/1024) + "KB"
+         else:
+             pass
+         if user.status == 'active':
+             if relativedelta(user.expired, datetime.utcnow()).months >= 1:
+                 left = ""
+             else:
+                 left = "({0} days left)".format(relativedelta(user.expired, datetime.utcnow()).days)
+             users.append({"id":user.id, "name":user.name, "payday":user.checkout, \
+                 "expired":user.expired.strftime("%y-%m-%d"), "left":left, "traffic":traffics})
+         else:
+             user.append({"id":user.id, "name":user.name, "payday":user.checkout, "expired":"NA", "left":0,"traffic":0})
+     return render_template("admin/main.html", users=users)
+
+
+@admin.route("/renewfee", methods=['GET', 'POST'])
+def admin_renewfee_user():
+    form = RenewfeeForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(name=form.name.data).first()
+        if user is None:
+            return redirect(url_for("admin.admin_main"))
+        if form.renew.data == '1m':
+            user.expired = user.expired + relativedelta(months=+1)
+        elif form.renew.data == '3m':
+            user.expired = user.expired + relativedelta(months=+3)
+        elif form.renew.data == '6m':
+            user.expired = user.expired + relativedelta(months=+6)
+        elif form.renew.data == '1y':
+            user.expired = user.expired + relativedelta(years=+1)
+        else:
+            pass
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("admin.admin_main"))
+    else:
+        return render_template("admin/renewfee.html", form=form)
 
 
 @admin.route("/createuser", methods=['GET', 'POST'])
@@ -38,7 +82,7 @@ def admin_create_user():
         else:
             pass
         user = User(name=form.name.data, password = form.password.data,
-            checkout = int(form.checkout.data),  package_id = int(form.package.data),
+            checkout = int(datetime.utcnow().day),  package_id = int(form.package.data),
             expired = datetime(expired.year, expired.month, expired.day, 0, 0, 0), status="active")
         db.session.add(user)
         db.session.commit()
@@ -59,6 +103,7 @@ def admin_start_user_log():
     db.session.add(userlog)
     db.session.commit()
     return "ok", 200
+
 
 @admin.route("/stopuserlog", methods=['POST'])
 def admin_stop_user_log():
